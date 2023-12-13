ee43fac332f901b051cea547c06c166ef13ed3e3
Anatoly <anatoly.kondrat@tktk.ee>
2023-12-13 10:35:16 +0200

    first commit

---
+++ 1program/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+Console.WriteLine("Enter your name:");
+
+string userName = Console.ReadLine();
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+Console.WriteLine($"Hello, {userName} !");
